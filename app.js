3f036d0478e79e66772f1be68ffced6bf557bfe3
cgitm <cg.itm.as@gmail.com>
2022-05-22 12:52:30 +0400

    initial

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ function showAds()
 {
 
 	var f = vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"})
-		.then(data => console.log(data.result))
-    		.catch(error => console.log(error));
+		  .then(data => console.log(data.result))
+    		  .catch(error => console.log(error));
     	console.log(f)
     	if (f)
     	{
     		vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
-    			.then(data => console.log(data.result))
-    			.catch(error => console.log(error));
+    		  .then(data => console.log(data.result))
+    		  .catch(error => console.log(error));
     	}
 }
 function vibro()
 {
 	vkBridge.send("VKWebAppTapticImpactOccurred", {"style": "heavy"});
+}
+function showLeaderBoard()
+{
+	vkBridge.send("VKWebAppShowLeaderBoardBox", {user_result:100})
+         .then(data => console.log(data.success))  
+         .catch(error => console.log(error));
 }
