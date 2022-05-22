1959f328e4a24fd1e2a8a6381d4023bffff7b7f0
cgitm <cg.itm.as@gmail.com>
2022-05-22 11:44:00 +0400

    initial

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-var user_name = 'test_user_name'
-
-function publish() {
-	alert("Hello");
-}
-function getAuthToken()
+function init()
 {
 	vkBridge.send('VKWebAppInit');
-	vkBridge.send("VKWebAppAddToFavorites");
-	console.log("Its OKAY!");
 }
-function showOrderBox()
+function showAds()
 {
-	vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"})
+
+	var f = vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"})
 		.then(data => console.log(data.result))
     		.catch(error => console.log(error));
-	vkBridge.send("VKWebAppCheckNativeAds", {"ad_format": "reward"})
-		.then(data => console.log(data.result))
-    		.catch(error => console.log(error));
-    	vkBridge.send("VKWebAppShowNativeAds", {ad_format:"reward"})
-    		.then(data => console.log(data.result))
-    		.catch(error => console.log(error));
+    	console.log(f)
+    	if (f)
+    	{
+    		vkBridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
+    			.then(data => console.log(data.result))
+    			.catch(error => console.log(error));
+    	}
 }
 function vibro()
 {
