18a31a7a30f85380d4601fc29d7d54ebad8dc00c
cgitm <cg.itm.as@gmail.com>
2022-05-28 17:34:03 +0400

    leader

--- app.js
+++ app.js
@@ -31,3 +31,12 @@ function addToGlobalLeaderBoard()
 	 .then(data => console.log(data.result))
 	 .catch(error => console.log(error));
 }
+
+function getConfig()
+{
+	vkBridge.send("VKWebAppGetConfig");
+	vkBridge.subscribe((e) => {
+		if(e.type == ‘VKWebAppUpdateConfig’) {
+  			console.log(e.data.status);
+		}});
+}
